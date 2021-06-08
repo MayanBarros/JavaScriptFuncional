c2a34d8e2359ab2510026da27895b1f3cff0884f
MAYAN BARROS DA SILVA <mayanbarrosdasilva@gmail.com>
2021-06-08 11:06:30 -0300

    Imutabilidade 2!!

---
+++ funcional/imutabilidade2.js
@@ -0,0 +1,32 @@
+const nums = [4, 8, 3, 2, 9, 1, 3]
+
+// Estratégia 3 com Recursividade
+
+function somarArray(array, total = 0) {
+    if(array.length === 0){
+        return total
+    }
+    return somarArray(array.slice(1), total + array[0])
+}
+
+const total = somarArray(nums)
+console.log(total)
+
+
+// Estratégia 2 com Reduce
+
+// const somar = (a, b) => a + b
+// const total = nums.reduce(somar)
+// console.log(total)
+
+
+
+//Estratégia 1 com dados mutáveis
+
+// let total = 0
+
+// for(let i = 0 ; i < nums.length ; i++) {
+//     total += nums[i]
+// }
+
+//console.log(total)
